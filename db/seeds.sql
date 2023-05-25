2a627a17a5f7c30c8f6a328886eae11f8cc7a58a
thoak90codes <adorahtho@gmail.com>
2023-05-24 23:02:31 -0500

    added database and tables to schema.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO department (name)
+VALUES ("Front Office"),
+("Housekeeping"),
+("Sales"),
+("Event Management"),
+("Management"),
+("Food & Beverage");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ()
