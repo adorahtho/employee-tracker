65d5c30fb7889c9b7d148179306f7af382f21b5e
thoak90codes <adorahtho@gmail.com>
2023-05-25 22:49:15 -0500

    added seedings to seeds.sql file

---
+++ db/query.sql
@@ -0,0 +1,4 @@
+SELECT employee.first_name, employee.last_name AS Manager
+FROM employee
+
+-- need to combine first & last name of employee and create a manager column
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,25 @@
 INSERT INTO department (name)
-VALUES ("Front Office"),
-("Housekeeping"),
-("Sales"),
-("Event Management"),
-("Management"),
-("Food & Beverage");
+VALUES ("Engineering"),
+("Finance"),
+("Legal"),
+("Sales");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ()
+VALUES ("Sales Lead", 100000, 4),
+("Salesperson", 80000, 4),
+("Lead Engineer", 150000, 1),
+("Software Engineer", 120000, 1),
+("Account Manager", 160000, 2),
+("Accountant", 125000, 2),
+("Legal Team Lead", 250000, 3),
+("Lawyer", 190000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("John", "Doe", 1),
+("Mike", "Chan", 2),
+("Ashley", "Rodriguez", 3),
+("Kevin", "Tupik", 4),
+("Kunal", "Singh", 5),
+("Malia", "Brown", 6),
+("Sarah", "Lourd", 7),
+("Tom", "Allen", 8);
